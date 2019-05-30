922aa7abbc04945cdfdf32b38a274a1a85ca9836
cfoulidis <cfoulidis@CFOULIDIS-PC>
2019-05-30 16:12:01 +0300

    adding files:

---
+++ Assets/Scripts/Managers/ScriptableObjectSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
+{ 
+  private static T _instance = null;
+
+public static T Instance
+{
+        get
+        {
+            if(_instance== null)
+            {
+                T[] resaults = Resources.FindObjectsOfTypeAll<T>();
+                if (resaults.Length == 0)
+                {
+                    Debug.LogError("SingletonSCriptableObject -> resault leangth is 0 for type "+ typeof(T).ToString() + ".");
+                    return null;
+                }
+                if (resaults.Length > 1)
+                {
+                    Debug.LogError("SingletonSCriptableObject -> resault leangth is more than 1 for type " + typeof(T).ToString() + ".");
+                    return null;
+                }
+                _instance = resaults[0];
+            }
+            return _instance;
+        }
+}
+}
+
+      
---
+++ Assets/TestConnect.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("connecting to server");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("connected to master");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("disconected from server for reason" + cause.ToString());
+    }
+
+}
